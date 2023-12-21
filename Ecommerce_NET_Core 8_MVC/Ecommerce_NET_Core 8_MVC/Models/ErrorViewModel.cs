65130cf02e119704ca42ac0ec4529d4e7d3e855a
LHHT-DISCOVERY <lytri102@gmail.com>
2023-12-21 23:07:11 +0700

    up document GraphDB , RDF vs Spring boot

---
+++ Ecommerce_NET_Core 8_MVC/Ecommerce_NET_Core 8_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ecommerce_NET_Core_8_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
